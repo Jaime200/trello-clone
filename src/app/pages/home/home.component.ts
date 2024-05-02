0da5081b7199c35eae3b8803db7e47b83e9b495a
Jaime Muñoz <jaime.munoz200@gmail.com>
2024-05-01 20:07:30 -0600

    Implementando accordion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,13 +4,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { 
   faBox,
   faWaveSquare,
-  faClock
+  faClock,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faGear,
+  faAngleUp,
+  faAngleDown
+
 } from '@fortawesome/free-solid-svg-icons'
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NavbarComponent, FontAwesomeModule ],
+  imports: [ NavbarComponent, FontAwesomeModule , CdkAccordionModule, CommonModule],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
@@ -18,4 +27,10 @@ export class HomeComponent {
   public faBox = faBox
   public faWaveSquare = faWaveSquare
   public faClock = faClock
+  public faHeart =faHeart
+  public faBorderAll =faBorderAll
+  public faUsers =faUsers
+  public faGear =faGear
+  public faAngleUp= faAngleUp
+  public faAngleDown= faAngleDown
 }
