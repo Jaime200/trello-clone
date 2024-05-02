b44ed573c6809e6464e5e4d3dbca6a891f99efd4
Jaime Muñoz <jaime.munoz200@gmail.com>
2024-05-01 20:16:48 -0600

    multiple accordiones

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -33,4 +33,40 @@ export class HomeComponent {
   public faGear =faGear
   public faAngleUp= faAngleUp
   public faAngleDown= faAngleDown
+
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1'
+        },
+        {
+          label: 'Sub Item 1.2'
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1'
+        }
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1'
+        },
+        {
+          label: 'Sub Item 3.2'
+        },
+        {
+          label: 'Sub Item 3.3'
+        }
+      ]
+    }
+  ]
 }
